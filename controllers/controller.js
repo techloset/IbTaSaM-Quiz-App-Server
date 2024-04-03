1b02d4607c6fda55dae664a5f5fcf0f11e14bc48
ibtasam116 <ibtasam116@gmail.com>
2024-04-03 09:13:55 +0500

    Remove extra code

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,7 +1,5 @@
-import Questions from "../models/questionSchema.js";
 import Results from "../models/resultSchema.js";
 import NextQuestion from "../models/nextQuestionSchema.js";
-import questions, { answers } from "../database/data.js";
 
 // get all questions
 export async function getQuestions(req, res) {
@@ -14,30 +12,50 @@ export async function getQuestions(req, res) {
 }
 
 // insert all questions
-// export async function insertQuestions(req, res) {
-//   try {
-//     await Questions.insertMany({ questions, answers })
-//       .then(function () {
-//         // console.log("Successfully Inserted!");
-//         res.json({ msg: "Data Saved Successfully...!" });
-//       })
-//       .catch(function (err) {
-//         console.log(err);
-//       });
-//   } catch (error) {
-//     res.json({ error });
-//   }
-// }
+
+export async function insertQuestion(req, res) {
+  try {
+    const { questions, answers } = req.body;
+
+    NextQuestion.findOneAndUpdate(
+      {},
+      {
+        $push: { questions: { $each: questions }, answers: { $each: answers } },
+        // $push: { answers: { $each: answers } },
+      },
+      { upsert: true }
+    )
+      .then(function (err, data) {
+        res.json({ msg: "Next Saved Successfully...!" });
+      })
+      .catch(function (err) {
+        console.log(err);
+        res.json({ msg: "erro" });
+      });
+  } catch (error) {
+    res.json({ error });
+    res.json({ msg: "erro" });
+  }
+}
 
 // Delete all questions
-// export async function dropQuestions(req, res) {
-//   try {
-//     await NextQuestion.deleteMany();
-//     res.json({ msg: "Questions Deleted Successfully...!" });
-//   } catch (error) {
-//     res.json({ error });
-//   }
-// }
+
+export async function dropQuestion(req, res) {
+  try {
+    const { id } = req.body;
+    const result = await NextQuestion.updateOne(
+      {},
+      { $pull: { questions: { id: id } } }
+    );
+    if (result.modifiedCount > 0) {
+      res.json("Question deleted successfully.");
+    } else {
+      res.json("Question with the provided ID was not found.");
+    }
+  } catch (error) {
+    res.json({ error });
+  }
+}
 
 // get all result
 export async function getResult(req, res) {
@@ -68,61 +86,11 @@ export async function storeResult(req, res) {
 }
 
 // Delete all result
-// export async function dropResult(req, res) {
-//   try {
-//     await Results.deleteMany();
-//     res.json({ msg: "Result Deleted Successfully...!" });
-//   } catch (error) {
-//     res.json({ error });
-//   }
-// }
-
-export async function insertQuestion(req, res) {
-  try {
-    const { questions, answers } = req.body;
-
-    NextQuestion.findOneAndUpdate(
-      {},
-      {
-        $push: { questions: { $each: questions }, answers: { $each: answers } },
-        // $push: { answers: { $each: answers } },
-      },
-      { upsert: true }
-    )
-      .then(function (err, data) {
-        res.json({ msg: "Next Saved Successfully...!" });
-      })
-      .catch(function (err) {
-        console.log(err);
-        res.json({ msg: "erro" });
-      });
-  } catch (error) {
-    res.json({ error });
-    res.json({ msg: "erro" });
-  }
-}
 
 export async function dropResult(req, res) {
   try {
     await Results.findOneAndDelete({ _id: "6607db5705a3088c87cd379a" });
     res.json({ msg: "Result Deleted Successfully...!" });
-  } catch (error) {
-    res.json({ error });
-  }
-}
-
-export async function dropQuestion(req, res) {
-  try {
-    const { id } = req.body;
-    const result = await NextQuestion.updateOne(
-      // {},
-      { $pull: { questions: { id: id } } }
-    );
-    if (result.modifiedCount > 0) {
-      res.json("Question deleted successfully.");
-    } else {
-      res.json("Question with the provided ID was not found.");
-    }
   } catch (error) {
     res.json({ error });
   }
--- database/conn.js
+++ database/conn.js
@@ -6,6 +6,8 @@ export default async function connect() {
     console.log("no");
     return;
   }
-  mongoose.connect("mongodb+srv://ibtasam116:ibtasam1234@cluster0.hc5uren.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
+  mongoose.connect(
+    "mongodb+srv://ibtasam116:ibtasam1234@cluster0.hc5uren.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+  );
   console.log("Connection Done");
 }
